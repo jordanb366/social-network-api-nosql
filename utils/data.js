7e0c42ae8398e659cf4385b1e7dd03a24d0a349d
Jordan <jordan.b3360@gmail.com>
2022-10-20 16:46:27 -0500

    worked on adding seeds, need to start testing routes with insomnia

--- utils/data.js
+++ utils/data.js
@@ -1,1 +1,119 @@
 // Seeds will go here for data
+
+const users = [
+  "Aaran",
+  "Aaren",
+  "Aarez",
+  "Aarman",
+  "Aaron",
+  "Aaron-James",
+  "Aarron",
+  "Aaryan",
+  "Aaryn",
+  "Aayan",
+  "Aazaan",
+  "Abaan",
+  "Abbas",
+  "Abdallah",
+  "Abdalroof",
+  "Abdihakim",
+  "Abdirahman",
+  "Abdisalam",
+  "Abdul",
+  "Abdul-Aziz",
+  "Abdulbasir",
+  "Abdulkadir",
+  "Abdulkarem",
+  "Smith",
+  "Jones",
+  "Coollastname",
+  "enter_name_here",
+  "Ze",
+  "Zechariah",
+  "Zeek",
+  "Zeeshan",
+  "Zeid",
+  "Zein",
+  "Zen",
+  "Zendel",
+  "Zenith",
+  "Zennon",
+  "Zeph",
+  "Zerah",
+  "Zhen",
+  "Zhi",
+  "Zhong",
+  "Zhuo",
+  "Zi",
+  "Zidane",
+  "Zijie",
+  "Zinedine",
+  "Zion",
+  "Zishan",
+  "Ziya",
+  "Ziyaan",
+  "Zohaib",
+  "Zohair",
+  "Zoubaeir",
+  "Zubair",
+  "Zubayr",
+  "Zuriel",
+  "Xander",
+  "Jared",
+  "Courtney",
+  "Gillian",
+  "Clark",
+  "Jared",
+  "Grace",
+  "Kelsey",
+  "Tamar",
+  "Alex",
+  "Mark",
+  "Tamar",
+  "Farish",
+  "Sarah",
+  "Nathaniel",
+  "Parker",
+];
+
+const userThoughts = [
+  "Decision Tracker",
+  "Find My Phone",
+  "Learn Piano",
+  "Starbase Defender",
+  "Tower Defense",
+  "Monopoly Money Manager",
+  "Movie trailers",
+  "Hello world",
+  "Stupid Social Media App",
+  "Notes",
+  "Messages",
+  "Email",
+  "Compass",
+  "Firefox",
+  "Running app",
+  "Cooking app",
+  "Poker",
+  "Deliveries",
+];
+
+// Get a random item given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Gets a random user
+const getRandomUser = () =>
+  `${getRandomArrItem(user)} ${getRandomArrItem(user)}`;
+
+// Function to generate random thoughts
+const getRandomThoughts = (int) => {
+  const results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughts: getRandomArrItem(userThoughts),
+    });
+  }
+  return results;
+};
+
+// Export the functions for use in seed.js
+module.exports = { getRandomUser, getRandomThoughts };
